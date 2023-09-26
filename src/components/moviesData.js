d156ed474f78a001e5354d8d6024358e1c7d5570
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-09-26 10:34:40 +0100

    completed all the tasks

---
+++ src/components/moviesData.js
@@ -0,0 +1,36 @@
+const moviesData = [
+	{
+		title: "Spider Man (2002)",
+		description: "Description 1",
+		trailerLink:
+			"https://www.youtube.com/embed/t06RUxPbp_c?si=fOtXSR38pqkhsPex",
+		image:
+			"https://imgs.search.brave.com/B87wCwXZLWIocOUVff6nSjlVb1AQQfF34diefaRl5wI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLmV0/c3lzdGF0aWMuY29t/LzI4OTExNzg1L2Mv/NTE5LzUxOS8wLzAv/aWwvN2Y3YWQ3LzUx/MjEzNDUyMjIvaWxf/NjAweDYwMC41MTIx/MzQ1MjIyX2p0eDAu/anBn",
+	},
+	{
+		title: "Oppenheimer",
+		description: "Description 2",
+		trailerLink:
+			"https://www.youtube.com/embed/uYPbbksJxIg?si=XMapXhVVPxaBNI7g",
+		image:
+			"https://imgs.search.brave.com/b6KJZsNymExB1wK8ITek-6nn1G1FHFe6OvRIRzw6RhQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tb3Zp/ZXMudW5pdmVyc2Fs/cGljdHVyZXMuY29t/L21lZGlhL29wcGVu/aGVpbWVyLXBvc3Rl/ci01NjB4ODgwLTYy/ZGVmYWNiMWIwMDIt/MS5qcGc",
+	},
+	{
+		title: "The Last - Naruto the Movie",
+		description: "description",
+		trailerLink:
+			"https://www.youtube.com/embed/tA3yE4_t6SY?si=Rhs-PnzaJ-e5SrfA",
+		image:
+			"https://imgs.search.brave.com/bia2viOfRo8ulNPfFXhUrdHlCsdT8xFh0MrZg284Oag/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOGE1/ZDE4ODMtOTg0YS00/YWM4LWEwYTUtZGNm/YTRlODc4ZDU3LmI2/OGM4N2E0ODUxNWU4/YTk1ZDBiZWVlN2I5/NjljZDYwLmpwZWc_/b2RuSGVpZ2h0PTYx/MiZvZG5XaWR0aD02/MTImb2RuQmc9RkZG/RkZG",
+	},
+	{
+		title: "One Piece Film Red",
+		description: "Description 2",
+		trailerLink:
+			"https://www.youtube.com/embed/eU0i7L3cakI?si=PGafdZFUV-caCpk3",
+		image:
+			"https://imgs.search.brave.com/mkLKAw8JjmgUSCLjzm9WOfCaOIR6R8XtuhXJ0hPN1_g/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zcG9y/dHNodWIuY2JzaXN0/YXRpYy5jb20vaS8y/MDIyLzA2LzA4LzNj/YTBlMDdiLWViNDAt/NGQ5Mi1hYTE4LWQ1/M2VlNjc0ODEzMC9v/bmUtcGllY2UtcmVk/LmpwZz9hdXRvPXdl/YnAmd2lkdGg9MTQ0/NiZoZWlnaHQ9MjA0/OCZjcm9wPTAuNzA2/OjEsc21hcnQ",
+	},
+];
+
+export default moviesData;
